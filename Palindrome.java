cb1c157dc6a9e0835c95ab803fdb8555c995e2ac
PravinShejul <PravinShejul@users.noreply.github.com>
2016-12-19 15:09:27 +0530

    Create Palindrome.java

---
+++ Palindrome.java
@@ -0,0 +1,26 @@
+//package com.company;
+
+public class Palin {
+
+    public static void main(String args[])
+    {
+        int n=818;
+
+        int rev=0;
+
+        hile(n>0)
+        
+            int r=n%10;
+            n=n/
+            rev= r*10 +rev;
+        }
+        if(rev==n)
+        {
+            System.out.println("palindrom")
+        }
+        else
+        
+            System.out.println("not palin");
+        }
+    }
+}
